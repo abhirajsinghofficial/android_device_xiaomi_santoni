f608e7ec53f50ca01ba14ce7bcc50542c345786e
Abhiraj <abhirajsinghofficial@gmail.com>
2018-07-12 16:44:45 +0530

    Add Recorder and Eleven

--- device.mk
+++ device.mk
@@ -140,7 +140,9 @@ PRODUCT_PACKAGES += \
     camera.msm8937 \
     libmm-qcamera \
     mm-qcamera-app \
-    Snap
+    Snap \
+    Recorder \
+    Eleven
 
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
